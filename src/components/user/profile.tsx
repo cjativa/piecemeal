df12707904e9ce0f3989540dae8d47128649bb6d
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-02-28 23:06:27 -0500

    capturing field values

--- src/components/user/profile.tsx
+++ src/components/user/profile.tsx
@@ -1,30 +1,42 @@
 import React, { useState } from 'react';
 
 export const Profile = () => {
+
+    const [title, setTitle] = useState('');
+    const [bio, setBio] = useState('');
+    const [company, setCompany] = useState('');
+    const [location, setLocation] = useState('');
+    const [website, setWebsite] = useState('');
+
+    const onChange = (set: (value: string) => void, event: any) => {
+        const { value } = event.target;
+        set(value);
+    }
+
     return (
         <div className="profile inner-container">
             <div className="profile__content">
                 <p>Profile</p>
                 <div className="field field--row">
                     <label>Title</label>
-                    <input type="text" placeholder="Software Engineer, UX Developer, etc." />
+                    <input type="text" name="title" onChange={(e) => onChange(setTitle, e)} placeholder="Software Engineer, UX Developer, etc." value={title} />
                 </div>
                 <div className="field field--row">
                     <label>Bio</label>
-                    <textarea rows={4} cols={25} placeholder="Write a few words about yourself">
+                    <textarea name="bio" onChange={(e) => onChange(setBio, e)} rows={4} cols={25} placeholder="Write a few words about yourself" value={bio}>
                     </textarea>
                 </div>
                 <div className="field field--col">
                     <label>Company</label>
-                    <input type="text" placeholder="Company" />
+                    <input type="text" name="company" onChange={(e) => onChange(setCompany, e)} placeholder="Company" value={company} />
                 </div>
                 <div className="field field--col">
                     <label>Location</label>
-                    <input type="text" placeholder="Location" />
+                    <input type="text" name="location" onChange={(e) => onChange(setLocation, e)} placeholder="Location" value={location} />
                 </div>
                 <div className="field field--col">
                     <label>Website</label>
-                    <input type="text" placeholder="Website" />
+                    <input type="text" name="website" onChange={(e) => onChange(setWebsite, e)} placeholder="Website" value={website} />
                 </div>
                 <div className="field--right">
                     <button>Save</button>
